a48fd8140c59dd20b0fe5f6b574c95d5a47de270
minsung <qkcvb110@naver.com>
2023-01-05 02:11:36 +0000

    test

--- 0103/test2.py
+++ 0103/test2.py
@@ -1,7 +1,29 @@
-n=int(input('숫자를 입력해주세요.:'))
-sum = 0
-for i in range(0, n+1, 2):
-    # print(i)
-    if i%2 == 0:
-        sum+= i
-print(sum)
+
+import RPi.GPIO as GPIO
+
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+LED1 = 11
+LED2 = 12
+LED3 = 13
+Led= [LED1,LED2,LED3]
+# GPIO.setup(LED1, GPIO.OUT, initial=GPIO.LOW)
+# GPIO.setup(LED2, GPIO.OUT, initial=GPIO.LOW)
+# GPIO.setup(LED3, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(Led, GPIO.OUT, initial=GPIO.LOW)
+while 1:
+    GPIO.output(LED1, GPIO.HIGH)
+    time.sleep(1)   
+    GPIO.output(LED1, GPIO.LOW)
+    time.sleep(1)  
+    GPIO.output(LED2, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(LED2, GPIO.LOW)
+    time.sleep(1)  
+    GPIO.output(LED3, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(LED3, GPIO.LOW)
+    time.sleep(1)
