acc4397ca759cccce5b41dd969a18b056ebd8d85
minsung <qkcvb110@naver.com>
2023-01-05 07:02:58 +0000

    test

---
+++ GIT/servoFND2.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as GPIO 
+import time  
+def main():
+    duty_ratio= 0
+    MaxDuty= 12
+    PWMpin= 12
+    GPIO.setmode(GPIO.BOARD) 
+    GPIO.setup(PWMpin, GPIO.OUT) 
+    GPIO.setwarnings(False)
+
+    seg = [40,38,32,26,24,22,18,36] # GPIO pin
+    GPIO.setup(seg, GPIO.OUT, initial=GPIO.LOW)
+
+    fnd = [(1,1,1,1,1,0,1,0), #0
+        (0,1,1,0,0,0,0,0), #1
+        (1,1,0,1,1,1,0,0), #2
+        (1,1,1,1,0,1,0,0), #3 
+        (0,1,1,0,0,1,1,0), #4
+        (1,0,1,1,0,1,1,0), #5
+        (1,0,1,1,1,1,1,0), #6
+        (1,1,1,0,0,0,1,0), #7
+        (1,1,1,1,1,1,1,0), #8
+        (1,1,1,0,0,1,1,0), #9
+        (1,1,1,1,1,0,1,0), #1
+        (0,1,1,0,0,0,0,0), #2
+        (1,1,0,1,1,1,0,0)] #3
+
+    Servo=GPIO.PWM(PWMpin, 50) 
+    Servo.start(0)
+    print('Wating for 1 sec') 
+    time.sleep(1) 
+    #print('Rotating at interval of 0-12 degrees’)
+    while duty_ratio <= MaxDuty:
+        duty_ratio= int(input('Enter Brightness (0~12):'))
+        Servo.ChangeDutyCycle(duty_ratio)
+        GPIO.output(seg,fnd[duty_ratio])
+       
+
+        if duty_ratio > MaxDuty:
+            duty_ratio= 0
+            Servo.ChangeDutyCycle(duty_ratio)
+            GPIO.output(seg,fnd[duty_ratio])
+        
+
+    Servo.stop()
+    GPIO.cleanup()
+    print('Everythings cleanup')
+if __name__ == '__main__':
+    main()
---
+++ GIT/test.py
@@ -0,0 +1,5 @@
+num=int(input('숫자를 입력해주세요.:'))
+if num > 10:
+    print('10보다 크다.')
+else:
+    print('10보다 작다.')
